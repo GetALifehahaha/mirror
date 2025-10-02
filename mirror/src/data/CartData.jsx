c90b1c8f55d750e881bdb2e6b33dbf941f4a8a5f
GetALifehahaha <sencioahlannour@gmail.com>
2025-10-02 21:38:31 +0800

    Create cart page, display current cart, add checkout logic + gross total amount

---
+++ mirror/src/data/CartData.jsx
@@ -0,0 +1,14 @@
+export const CartData = [
+    {
+        id: 0,
+        productName: 'Apple',
+        productPrice: 1.00,
+        amount: 2,
+    },
+    {
+        id: 1,
+        productName: 'Banana',
+        productPrice: 3.00,
+        amount: 4,
+    },
+]
--- mirror/src/pages/Cart.jsx
+++ mirror/src/pages/Cart.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { CartData } from '../data/CartData'
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
+
+  const [productsCheckout, setProductsCheckout] = useState([]);
+  const [grossTotal, setGrossTotal] = useState(0);
+
+  const navigate = useNavigate();
+
+  const listCartProducts = CartData.map(product => 
+    <div key={product.id}>
+      <button onClick={() => handleProductsForCheckout(product)}>Add</button>
+      <h5 onClick={() => navigate(`/products/${product.id}`)}>{product.productName} : </h5>
+      <h5>{product.productPrice}</h5>
+    </div>
+  );
+
+  const listProductsForCheckout = productsCheckout.map(product =>
+    <div key={product.id}>
+      <h5>{product.productName} : </h5>
+      <h5>{product.productPrice}</h5>
+    </div>
   )
+
+  useEffect(() => {
+    handleSetGrossTotal();
+  }, [productsCheckout])
+
+  const handleSetGrossTotal = () => {
+    setGrossTotal(0);
+
+    productsCheckout.forEach(product => setGrossTotal(prevGT => prevGT + product.productPrice));
+  }
+
+  const handleProductsForCheckout = (product) => {
+
+    setProductsCheckout(pc => {
+      const productInCheckout = pc.some(p => p.id === product.id);
+
+      if (productInCheckout) {
+        return pc.filter(p => p.id !== product.id);
+      } else {
+        return [...pc, product];
+      }
+    });
+  } 
+
+  return (
+    <div>{listCartProducts}
+    
+      <div>
+        {listProductsForCheckout}
+        {grossTotal}
+      </div>
+    </div>
+  );
 }
 
 export default Cart
